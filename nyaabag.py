9a376a194db42e0d336ef84af17544b25ca9ae60
Dinank <vashishthdinank11@gmail.com>
2021-09-07 17:40:30 +0530

    Updated nyaabag.py
    Users can now search with either english or japanese anime name, added comments for code clarity, and a bit of code refactoring.

--- nyaabag.py
+++ nyaabag.py
@@ -2,12 +2,13 @@ import requests
 import re
 from fuzzywuzzy import fuzz,process
 import pandas as pd
-def get_nyaa_data(name_eng,name_jap,group):
-    name_eng = re.sub('\(.*?\)','', name_eng)
-    name_jap = re.sub('\(.*?\)','', name_jap)
+
+
+def get_nyaa_data(name,group):
+    name_eng = re.sub('\(.*?\)','', name)
     from nyaa import Nyaa
     Nyaa=Nyaa()
-    searching = Nyaa.search(keyword=group+" "+name_eng, category=1)
+    searching = Nyaa.search(keyword=group+" "+name, category=1)
     for torrent in searching:
         print("-----------------------------------------------------------")
         print("Title: "+str(torrent.name))
@@ -20,24 +21,48 @@ def get_nyaa_data(name_eng,name_jap,group):
         print()
 
 
-#sheet_url="https://docs.google.com/spreadsheets/d/1emW2Zsb0gEtEHiub_YHpazvBd4lL4saxCwyPhbtxXYM/edit#gid=0"
+"""
+Network url is commented because we are using a local copy of it
+Uncomment the network url, and comment the local file name line, to use it.
+"""
 sheet_url="index_seadex.csv"
+#sheet_url="https://docs.google.com/spreadsheets/d/1emW2Zsb0gEtEHiub_YHpazvBd4lL4saxCwyPhbtxXYM/edit#gid=0"
+
+#changing the google sheet link to get in csv format
 url_1 = sheet_url.replace('/edit#gid=', '/export?format=csv&gid=')
+
+#ignoring first line of the csv to use as headers
 best_release_data = pd.read_csv(url_1,header=1,index_col=[0])
 print("**WELCOME TO SEADEX TO NYAA FETCHER**")
 user_anime_name = input("Please enter the name of the anime you want to search: ")
 print('******************************************************')
-best_matches = process.extract(user_anime_name, best_release_data['Title'].tolist(), limit = 5, )
+
+#fuzzily matching the user input to index anime titles, and fetching 5 entries each from title and  alternate title
+fuzzy_result_limit=5
+best_matches = process.extract(user_anime_name, best_release_data['Title'].tolist(), limit = fuzzy_result_limit, )
+best_matches.extend(process.extract(user_anime_name, best_release_data['Alternate Title'].tolist(), limit = fuzzy_result_limit, ))
 for i in range(len(best_matches)):
-    print(str(i+1)+". "+best_matches[i][0])
+    print(str(i+1)+". "+str(best_matches[i][0]))
 print('******************************************************')
 anime_id=int(input("Choose your anime by entering a number: "))
 anime_id-=1
-anime_name=best_matches[anime_id][0]
 
-best_release_data.set_index("Title",inplace=True)
-chosen_row=best_release_data.loc[best_matches[anime_id][0]]
-anime_name_jap=str(chosen_row["Alternate Title"])
+#Searching with both keys, title and alternate title
+#Finally getting the japanese and english name, of the anime which user picked
+if 0<=anime_id<=4:
+    best_release_data.set_index("Title",inplace=True)
+    chosen_row=best_release_data.loc[best_matches[anime_id][0]]
+    anime_name=best_matches[anime_id][0]
+    anime_name_jap=str(chosen_row["Alternate Title"])
+elif 5<=anime_id<=9:
+    best_release_data.set_index("Alternate Title",inplace=True)
+    chosen_row=best_release_data.loc[best_matches[anime_id][0]]
+    anime_name=str(chosen_row["Title"])
+    anime_name_jap=str(best_matches[anime_id][0])
+else:
+    print("Given choice does not match any anime")
+#Fetching the best and alternate releases from the index of the chosen anime
+#Also, cleaning the result we get. Removing special symbols, taking care of season markers, splitting up multiple groups.
 releases=[]
 if(str(chosen_row["Best Release"]) != 'nan'):
     temp = str(chosen_row["Best Release"]).split()
@@ -55,7 +80,10 @@ if(str(chosen_row["Alternate Release"]) != 'nan'):
         new_string = re.sub('[^A-z0-9:\-_\+]+', '', s)
         if(new_string != ''):
             releases.append(new_string)
-#Now we have to extract the release group name from releases list
+
+#Parsing the season number / ova and release groups, and mapping them together.
+#Splitting the release groups if there are multiple mentioned with a + symbol.
+#Null string is mapped to release if no season/ova is specified.
 season_ova=[]
 group=[]
 i=0
@@ -90,12 +118,14 @@ while(i < len(releases)):
             group.append(first)
         i+=1
 
+#Displaying user a list of fetched seasons/ova/empty to release group mapping, and asking for release group choice.
 for i in range(len(season_ova)):
-    print(str(i+1)+". "+str(season_ova[i])+"\t"+group[i])
+    print(str(i+1)+". "+str(season_ova[i])+"\t"+str(group[i]))
 print('******************************************************')
 final_choice=int(input("Choose the season / release group name: "))
 final_choice-=1
 print('******************************************************')
-print("Fetching "+group[final_choice]+" release of "+str(anime_name_jap)+" / "+anime_name+" from nyaa.")
+print("Fetching "+str(group[final_choice])+" release of "+str(anime_name_jap)+" / "+str(anime_name)+" from nyaa.")
 
-get_nyaa_data(anime_name,anime_name_jap,group[final_choice])
+get_nyaa_data(anime_name,group[final_choice])
+get_nyaa_data(anime_name_jap,group[final_choice])
